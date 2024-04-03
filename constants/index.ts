a06d8421ed08cc6fd2e8d9077441f31932db6482
Aditya Yadav <103302549+adiyadav123@users.noreply.github.com>
2024-04-03 15:43:48 +0530

    main

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,7 @@ export const sidebarLinks = [
     {
         label: 'Home',
         route: '/',
-        imgUrl: '/icons/home.svg'
+        imgUrl: '/icons/Home.svg'
     },
     {
         label: 'Upcoming',
